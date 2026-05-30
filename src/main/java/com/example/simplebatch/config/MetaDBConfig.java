270696e1802a7086a2f40823458d2f37bf778de2
coding-faster <berichmore@naver.com>
2026-05-30 13:58:22 +0900

    Spring batch Settings

---
+++ src/main/java/com/example/simplebatch/config/MetaDBConfig.java
@@ -0,0 +1,32 @@
+package com.example.simplebatch.config;
+
+import org.springframework.boot.batch.jdbc.autoconfigure.BatchDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MetaDBConfig {
+
+    @Primary
+    @BatchDataSource
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource-meta")
+    public DataSource metaDBSource() {
+
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager metaTransactionalManager() {
+
+        return new DataSourceTransactionManager(metaDBSource());
+    }
+}
