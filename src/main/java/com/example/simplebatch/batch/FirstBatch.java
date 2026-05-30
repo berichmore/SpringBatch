fccf52d87565e6e8158a9847f44430c6e0e9a37c
coding-faster <berichmore@naver.com>
2026-05-30 16:43:15 +0900

    [Add] beforeReader() 추가

--- src/main/java/com/example/simplebatch/batch/FirstBatch.java
+++ src/main/java/com/example/simplebatch/batch/FirstBatch.java
@@ -1,14 +1,23 @@
 package com.example.simplebatch.batch;
 
 
+import com.example.simplebatch.entity.AfterEntity;
+import com.example.simplebatch.entity.BeforeEntity;
 import com.example.simplebatch.repository.AfterRepository;
 import com.example.simplebatch.repository.BeforeRepository;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.data.RepositoryItemReader;
+import org.springframework.batch.infrastructure.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Map;
 
 @Configuration
 public class FirstBatch {
@@ -40,6 +49,26 @@ public class FirstBatch {
 
     }
 
+    @Bean
+    public Step firstStep() {
+        return new StepBuilder("firstStep", jobRepository)
+                .<BeforeEntity, AfterEntity> chunk(10, platformTransactionManager)
+                .reader(beforeReader())
+                .processor()
+                .writer()
+                .build();
 
+    }
+
+    @Bean
+    public RepositoryItemReader<BeforeEntity> beforeReader() {
+        return new RepositoryItemReaderBuilder<BeforeEntity>()
+                .name("beforeReader")
+                .pageSize(10)
+                .methodName("findAll")
+                .repository(beforeRepository)
+                .sorts(Map.of("id", Sort.Direction.ASC))
+                .build();
+    }
 
 }
