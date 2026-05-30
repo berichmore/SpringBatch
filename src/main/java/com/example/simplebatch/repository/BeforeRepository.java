f225540b692c11fbd28f99e4cea832782914614c
coding-faster <berichmore@naver.com>
2026-05-30 16:11:52 +0900

    BeforeRepository Interface

---
+++ src/main/java/com/example/simplebatch/repository/BeforeRepository.java
@@ -0,0 +1,9 @@
+package com.example.simplebatch.repository;
+
+import com.example.simplebatch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+
+
+}
